b1edcc032005660d60e50ed35b5c64622fdab1de
arifpratama5 <arif.pratama@finansia.com>
2021-05-04 11:10:00 +0700

    Assignment 2

---
+++ database/db.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+	"github.com/labstack/gommon/log"
+)
+
+
+var(
+	host = "127.0.0.1"
+	user = "sqlserver"
+	password ="123456"
+	port = "1433"
+)
+var dbSQLServer *gorm.DB
+
+func StartDB() (*gorm.DB, error) {
+	dbmy,err :=gorm.Open("mssql",  "sqlserver://"+user+":"+password+"@"+host+":"+port+"?database=orders_by")
+	dbmy.DB().SetMaxIdleConns(10)
+	dbmy.LogMode(true)
+
+	if err != nil {
+		log.Error(err)
+		return nil,err
+	}
+	dbSQLServer  = dbmy
+	return dbSQLServer,nil
+}
+
+func GetDBPool() *gorm.DB {
+	return dbSQLServer
+}
